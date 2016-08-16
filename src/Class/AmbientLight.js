4daf27fba97c3f79a1ca2598ed25465619f61454
Olivier Monnier <omonnier.dev@gmail.com>
2016-08-16 17:54:55 +0200

    update light

--- src/Class/AmbientLight.js
+++ src/Class/AmbientLight.js
@@ -1,6 +1,17 @@
 export const AmbientLight = new WHS.AmbientLight({
   light: {
-    color: 0x0c0c0c,
-    intensity: 0.3
+    color: 0xffffff,
+    intensity: 0.2
+  },
+  pos: {
+    x: 160, // 100,
+    y: 120, // 30,
+    z: 160 // 100
+  },
+
+  target: {
+    x: 0,
+    y: 10,
+    z: 0
   }
 });
--- src/Class/SpotLight.js
+++ src/Class/SpotLight.js
@@ -1,12 +1,23 @@
 export const SpotLight = new WHS.SpotLight({
   light: {
     color: 0xffffff,
-    intensity: 3,
-    distance: 400
+    intensity: 0.3,
+    distance: 500
+  },
+  shadowmap: {
+    width: 2048,
+    height: 2048,
+    top: 0,
+    fov: 90
   },
   pos: {
-    x: 10,
-    y: 200,
-    z: 30
+    x: 160,
+    y: 120,
+    z: 160
+  },
+  target: {
+    x: 0,
+    y: 10,
+    z: 0
   }
 });
