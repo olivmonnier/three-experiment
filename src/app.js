6df7f075a4d58e7b9cffd84118bd5e685fc232a8
Olivier Monnier <omonnier.dev@gmail.com>
2016-08-17 18:06:08 +0200

    improve controls

--- src/app.js
+++ src/app.js
@@ -22,33 +22,42 @@ css3d.addElement({
   }
 });
 
-document.addEventListener('click', () => {
-  css3d.removeElement('hello');
-});
-
 AmbientLight.addTo(GAME);
 SpotLight.addTo(GAME);
 terrain.floor.forEach(ground => ground.addTo(GAME));
 
-GAME.add(Person).then(() => {
-  const checker2 = new WHS.Loop(() => {
-    if (Person.position.y < -200) {
-      Person.position.set(0, 100, 0);
-      Person.setAngularVelocity(new THREE.Vector3(0, 0, 0));
-      Person.setLinearVelocity(new THREE.Vector3(0, 0, 0));
-    }
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    const controls = new THREE.DeviceOrientationControls( GAME.camera._native );
+    const loopControls = new WHS.Loop(() => {
+      controls.update();
+    });
+    const blocker = document.getElementById('blocker');
+
+    blocker.style.display = "none";
+    GAME.addLoop(loopControls);
+    loopControls.start();
+} else {
+  GAME.add(Person).then(() => {
+    const checker2 = new WHS.Loop(() => {
+      if (Person.position.y < -200) {
+        Person.position.set(0, 100, 0);
+        Person.setAngularVelocity(new THREE.Vector3(0, 0, 0));
+        Person.setLinearVelocity(new THREE.Vector3(0, 0, 0));
+      }
+    });
+
+    GAME.addLoop(checker2);
+
+    checker2.start();
   });
 
-  GAME.addLoop(checker2);
+  GAME.setControls(
+    WHS.firstPersonControls(Person, {
+      speed: 10
+    })
+  );
+}
 
-  checker2.start();
-});
-
-GAME.setControls(
-  WHS.firstPersonControls(Person, {
-    speed: 10
-  })
-);
 
 GAME.addLoop(css3d.loop);
 GAME.addLoop(terrain.loop);
@@ -57,3 +66,34 @@ css3d.loop.start();
 terrain.loop.start();
 
 GAME.start();
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 13) {
+    toggleFullScreen();
+  }
+}, false);
+
+document.addEventListener('click', () => {
+  css3d.removeElement('hello');
+});
